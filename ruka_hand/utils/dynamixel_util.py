010a2d0be4597f98ce5842f2a652f41c89a1323d
pianapolataa <sissi.hu@hotmail.com>
2025-10-16 18:58:51 -0400

    .

--- ruka_hand/utils/dynamixel_util.py
+++ ruka_hand/utils/dynamixel_util.py
@@ -278,14 +278,12 @@ class DynamixelClient:
         """
         if len(motor_ids) > 3:
             groups = []
-            if any(m <= 3 for m in motor_ids):
-                groups.append([m for m in motor_ids if 1 <= m <= 3])
-            if any(4 <= m <= 6 for m in motor_ids):
-                groups.append([m for m in motor_ids if 4 <= m <= 6])
-            if any(7 <= m <= 9 for m in motor_ids):
-                groups.append([m for m in motor_ids if 7 <= m <= 9])
-            if any(10 <= m <= 11 for m in motor_ids):
-                groups.append([m for m in motor_ids if 10 <= m <= 11])
+            if any(m <= 4 for m in motor_ids):
+                groups.append([m for m in motor_ids if 1 <= m <= 4])
+            if any(5 <= m <= 8 for m in motor_ids):
+                groups.append([m for m in motor_ids if 5 <= m <= 8])
+            if any(9 <= m <= 11 for m in motor_ids):
+                groups.append([m for m in motor_ids if 9 <= m <= 11])
             if any(12 <= m <= 14 for m in motor_ids):
                 groups.append([m for m in motor_ids if 12 <= m <= 14])
             if any(15 <= m <= 16 for m in motor_ids):
