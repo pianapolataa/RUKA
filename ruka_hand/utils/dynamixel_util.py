863945c8af3234dfc2d085768b2569c6da24bb31
pianapolataa <sissi.hu@hotmail.com>
2025-10-16 19:08:04 -0400

    fix

--- ruka_hand/utils/dynamixel_util.py
+++ ruka_hand/utils/dynamixel_util.py
@@ -276,14 +276,12 @@ class DynamixelClient:
             address: The data's address in the control table to read from
             size: The length of the data being read
         """
-        if len(motor_ids) > 3:
+        if len(motor_ids) > 6:
             groups = []
-            if any(m <= 4 for m in motor_ids):
-                groups.append([m for m in motor_ids if 1 <= m <= 4])
-            if any(5 <= m <= 8 for m in motor_ids):
-                groups.append([m for m in motor_ids if 5 <= m <= 8])
-            if any(9 <= m <= 11 for m in motor_ids):
-                groups.append([m for m in motor_ids if 9 <= m <= 11])
+            if any(m <= 6 for m in motor_ids):
+                groups.append([m for m in motor_ids if 1 <= m <= 6])
+            if any(m <= 11 for m in motor_ids):
+                groups.append([m for m in motor_ids if 7 <= m <= 11])
             if any(12 <= m <= 14 for m in motor_ids):
                 groups.append([m for m in motor_ids if 12 <= m <= 14])
             if any(15 <= m <= 16 for m in motor_ids):
