4aab7b8a3336611f6a36b162ac3ec82cabe4e5aa
pianapolataa <sissi.hu@hotmail.com>
2025-11-17 09:53:45 -0500

    .

--- ruka_hand/control/rukav2_teleop.py
+++ ruka_hand/control/rukav2_teleop.py
@@ -147,7 +147,7 @@ class RUKAv2Handler:
         test_pos[2] = test_pos[2] - 0.07 * test_pos[0]
 
         test_pos[13] = (test_pos[13] - 45) * 2 # change thumb underestimation
-        test_pos[12] = test_pos[12] * 1.017 # change thumb angle
+        test_pos[12] = test_pos[12] * 1.027 # change thumb angle
         print(test_pos[12])
 
         clamped = np.clip(test_pos, min_deg, max_deg)
