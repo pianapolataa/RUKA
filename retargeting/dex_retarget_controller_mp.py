8d14ef7eba0ab1f84a7d614dd05822c2ff523df8
pianapolataa <sissi.hu@hotmail.com>
2026-02-02 14:39:15 -0500

    .

--- retargeting/dex_retarget_controller_mp.py
+++ retargeting/dex_retarget_controller_mp.py
@@ -153,7 +153,7 @@ class DexRukav2Handler:
         angles[0] = finger_deg['pinky_mcp']
         angles[2] = (finger_deg['pinky_pip'] + finger_deg['pinky_dip']) * 2 / 3
         angles[12] = finger_deg['thumb_cmc']
-        angles[13] = finger_deg['thumb_mcp']
+        angles[13] = finger_deg['thumb_mcp'] * 1.2
         angles[11] = finger_deg['thumb_ip']
         return np.degrees(angles)
 
