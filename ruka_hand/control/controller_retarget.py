27b5924b598667d61ee836af4d6434e3ae005a24
pianapolataa <sissi.hu@hotmail.com>
2026-02-02 14:36:33 -0500

    .

--- ruka_hand/control/controller_retarget.py
+++ ruka_hand/control/controller_retarget.py
@@ -144,10 +144,10 @@ class DexRukav2Handler:
         
         angles[7] = finger_deg['index_splay']
         # angles[8] = finger_deg['index_mcp'] * 1.27
-        angles[8] = finger_deg['index_mcp'] * 1.5
+        angles[8] = finger_deg['index_mcp'] * 1.7
         # angles[6] = (finger_deg['index_pip'] + finger_deg['index_dip']) * 2 / 3
         angles[6] = 0
-        angles[10] = finger_deg['mid_mcp']
+        angles[10] = finger_deg['mid_mcp'] * 1.3
         # angles[9] = (finger_deg['mid_pip'] + finger_deg['mid_dip']) * 2 / 3
         angles[9] = 0.17
         angles[3] = finger_deg['ring_splay']
