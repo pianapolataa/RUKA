4a86fb33886d5a13b20f68a0d0df300c01631aff
pianapolataa <sissi.hu@hotmail.com>
2026-03-02 14:55:29 -0500

    add left controller

--- ruka_hand/control/controller_retarget_left.py
+++ ruka_hand/control/controller_retarget_left.py
@@ -22,7 +22,7 @@ def apply_offset(pos, orn, offset):
     world_offset = rot_matrix.dot(offset)
     return pos + world_offset
 
-class DexRukav2Handler:
+class DexRukav2HandlerLeft:
     def __init__(self, urdf_path="/Users/sissi/Downloads/RUKA/assets/robot.urdf", config_path="/Users/sissi/Downloads/RUKA/assets/dex_retarget.yml", hand_type="right"):
         with open(config_path, "r") as f:
             self.config = yaml.safe_load(f)
